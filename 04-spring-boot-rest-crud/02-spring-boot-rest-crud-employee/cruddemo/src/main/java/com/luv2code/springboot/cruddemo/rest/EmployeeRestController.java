b1fa3c922cc7b2bb570a19afb3fe92b66e348200
ljubica.vlahovic <vlahovic.ljubica5@gmail.com>
2025-07-10 16:06:35 +0200

    get single, add and update

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.luv2code.springboot.cruddemo.rest;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,38 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
 
+        Employee theEmployee=employeeService.findById(employeeId);
+
+        if(theEmployee==null){
+            throw new RuntimeException("Employee id not found - "+employeeId);
+        }
+        return theEmployee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        // also just in case they pass an id in JSON ... set id to 0
+        // thid is to force a save of new item ... instaed of update
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee=employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+
+        Employee dbEmployee=employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
 }
